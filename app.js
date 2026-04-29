a65a54188b790b18e8dd2a8e8f34d6c5908b2e2b
Prathik <pprathikkulkarni@gmail.com>
2026-04-29 13:31:18 +0000

    test deployment change

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req,res)=>{
-  res.send("CI/CD Pipeline Running 🚀");
+  res.send("NEW DEPLOYMENT SUCCESS 🎉");
 });
 
 app.listen(3000);
